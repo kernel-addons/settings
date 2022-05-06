cef2cd73cb8f803f71a53b55c717afadc4bdf906
Swishilicous <paige.git@gmail.com>
2022-05-06 16:43:11 -0500

    Add KernelStorage to types

--- types.d.ts
+++ types.d.ts
@@ -8,6 +8,7 @@ declare const _: typeof import("lodash");
 
 // Our globals
 declare const SettingsNative: typeof import("./src/preload/native").default;
+declare const KernelStorage: typeof import("./src/renderer/modules/storage").Storage;
 declare const KernelSettings: typeof import("./src/renderer/modules/settings").SettingsRenderer;
 
 // Webpack compilation
